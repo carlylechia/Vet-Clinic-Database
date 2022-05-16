bc907c0021f0980b6e0b1314b78954e88b87e13f
Chia Carlyle <chiacarlyle@gmail.com>
2022-05-16 23:28:51 +0100

    Audit and improve performance for some queries

--- queries.sql
+++ queries.sql
@@ -163,3 +163,62 @@ SELECT species.name AS expected_specialty FROM animals JOIN visits ON animals.id
 --  expected_specialty 
 -- --------------------
 --  Digimon
+
+explain analyze SELECT COUNT(*) FROM visits where animal_id = 4;
+-- Finalize Aggregate  (cost=170348.29..170348.30 rows=1 width=8) (actual time=1000.501..1003.884 rows=1 loops=1)
+--    ->  Gather  (cost=170348.08..170348.29 rows=2 width=8) (actual time=1000.251..1003.862 rows=3 loops=1)
+--          Workers Planned: 2
+--          Workers Launched: 2
+--          ->  Partial Aggregate  (cost=169348.08..169348.09 rows=1 width=8) (actual time=981.041..981.043 rows=1 loops=3)
+--                ->  Parallel Seq Scan on visits  (cost=0.00..167854.46 rows=597446 width=0) (actual time=0.082..917.412 rows=479238 loops=3)
+--                      Filter: (animal_id = 4)
+--                      Rows Removed by Filter: 4792380
+--  Planning Time: 0.070 ms
+--  Execution Time: 1003.925 ms
+CREATE INDEX idx_animal_id ON visits(animal_id);
+ANALYZE;
+explain analyze SELECT COUNT(*) FROM visits where animal_id = 4;
+-- Finalize Aggregate  (cost=24255.56..24255.56 rows=1 width=8) (actual time=118.215..119.605 rows=1 loops=1)
+--    ->  Gather  (cost=24255.34..24255.55 rows=2 width=8) (actual time=118.048..119.596 rows=3 loops=1)
+--          Workers Planned: 2
+--          Workers Launched: 2
+--          ->  Partial Aggregate  (cost=23255.34..23255.35 rows=1 width=8) (actual time=107.662..107.662 rows=1 loops=3)
+--                ->  Parallel Index Only Scan using idx_animal_id on visits  (cost=0.43..21750.19 rows=602062 width=0) (actual time=0.151..70.470 rows=479238 loops=3)
+--                      Index Cond: (animal_id = 4)
+--                      Heap Fetches: 0
+--  Planning Time: 0.389 ms
+--  Execution Time: 119.881 ms
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+-- Seq Scan on visits  (cost=0.00..283171.65 rows=3877274 width=12) (actual time=1.745..1251.164 rows=3953717 loops=1)
+--    Filter: (vet_id = 2)
+--    Rows Removed by Filter: 11861135
+--  Planning Time: 0.101 ms
+--  Execution Time: 1463.933 ms
+CREATE INDEX idx_vet_id_asc ON visits (vet_id ASC);
+ANALYZE;
+EXPLAIN ANALYZE SELECT * FROM visits WHERE vet_id = 2;
+-- Finalize Aggregate  (cost=63890.91..63890.92 rows=1 width=8) (actual time=314.925..318.087 rows=1 loops=1)
+--    ->  Gather  (cost=63890.70..63890.91 rows=2 width=8) (actual time=314.693..318.078 rows=3 loops=1)
+--          Workers Planned: 2
+--          Workers Launched: 2
+--          ->  Partial Aggregate  (cost=62890.70..62890.71 rows=1 width=8) (actual time=302.808..302.809 rows=1 loops=3)
+--                ->  Parallel Index Only Scan using idx_vet_id_asc on visits  (cost=0.43..58820.02 rows=1628271 width=0) (actual time=0.064..187.248 rows=1317906 loops=3)
+--                      Index Cond: (vet_id = 2)
+--                      Heap Fetches: 0
+--  Planning Time: 0.096 ms
+--  Execution Time: 320.371 ms
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
+--  Gather  (cost=1000.00..142496.07 rows=4 width=43) (actual time=9.378..3144.924 rows=4 loops=1)
+--    Workers Planned: 2
+--    Workers Launched: 2
+--    ->  Parallel Seq Scan on owners  (cost=0.00..141495.67 rows=2 width=43) (actual time=1831.858..3133.374 rows=1 loops=3)
+--          Filter: ((email)::text = 'owner_18327@mail.com'::text)
+--          Rows Removed by Filter: 3333334
+--  Planning Time: 2.373 ms
+--  Execution Time: 3144.978 ms
+CREATE INDEX idx_email ON owners (email);
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
+-- Index Scan using idx_email on owners  (cost=0.43..20.44 rows=4 width=43) (actual time=2.942..2.997 rows=4 loops=1)
+--    Index Cond: ((email)::text = 'owner_18327@mail.com'::text)
+--  Planning Time: 0.597 ms
+--  Execution Time: 3.018 ms
